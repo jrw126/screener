75e31710438b89423402ab9bf751f26d463a7779
John Wright <jrw126@gmail.com>
2018-12-22 22:48:32 +0000

    Updates

--- scrn.R
+++ scrn.R
@@ -2,7 +2,7 @@
 # Fundamentals screener
 library(tidyverse)
 
-setwd("D:/Analysis/Screener")
+setwd("D:/Analysis/screener")
 
 # Last five years of data from here:
 # https://www.sec.gov/dera/data/financial-statement-data-sets.html
@@ -10,16 +10,19 @@ setwd("D:/Analysis/Screener")
 # Documentation here:
 # https://www.sec.gov/files/aqfs.pdf
 
-zipfiles <- grep("zip$", list.files(), value = T)
+datapath <- "D:/Data Sets/SEC"
+zipfiles <- grep("zip$", list.files(datapath), value = T)
 num <- pre <- sub <- tag <- data.frame()
 if (!file.exists("num.RData")) {
       for (z in zipfiles) {
             
+            z_dir <- paste0(datapath, "/", z)
+            
             # Read each file in the zip archives
-            num_tmp <- read_delim(unz(z, "num.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
-            pre_tmp <- read_delim(unz(z, "pre.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
-            sub_tmp <- read_delim(unz(z, "sub.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
-            tag_tmp <- read_delim(unz(z, "tag.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
+            num_tmp <- read_delim(unz(z_dir, "num.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
+            pre_tmp <- read_delim(unz(z_dir, "pre.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
+            sub_tmp <- read_delim(unz(z_dir, "sub.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
+            tag_tmp <- read_delim(unz(z_dir, "tag.txt"), "\t", escape_double = FALSE, trim_ws = TRUE)
             
             # Append to master dataframe
             num <- rbind(num, num_tmp)
@@ -35,3 +38,9 @@ if (!file.exists("num.RData")) {
       
       rm(num_tmp); rm(pre_tmp); rm(sub_tmp); rm(tag_tmp); gc()
 }
+
+library(edgar)
+getMasterIndex(c(2013:2018))
+
+report <- getFilings(2017, 0000320193, '10-Q')
+
